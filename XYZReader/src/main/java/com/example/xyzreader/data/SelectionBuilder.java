7dfe72e88ce63f44dd92f398333de07f0336018d
Pedro Mendes <pedro.mendes@peixeurbano.com>
2018-02-28 23:18:17 -0300

    Removing unused methods

--- XYZReader/src/main/java/com/example/xyzreader/data/SelectionBuilder.java
+++ XYZReader/src/main/java/com/example/xyzreader/data/SelectionBuilder.java
@@ -43,23 +43,6 @@ public class SelectionBuilder {
     private ArrayList<String> mSelectionArgs;
 
     /**
-     * Reset any internal state, allowing this builder to be recycled.
-     */
-    public SelectionBuilder reset() {
-        mTable = null;
-		if (mProjectionMap != null) {
-			mProjectionMap.clear();
-		}
-		if (mSelection != null) {
-			mSelection.setLength(0);
-		}
-		if (mSelectionArgs != null) {
-			mSelectionArgs.clear();
-		}
-        return this;
-    }
-
-    /**
      * Append the given selection clause to the internal state. Each clause is
      * surrounded with parenthesis and combined using {@code AND}.
      */
@@ -115,14 +98,8 @@ public class SelectionBuilder {
 
     private void ensureSelectionArgs() {
     	if (mSelectionArgs == null) {
-    		mSelectionArgs = new ArrayList<String>();
+    		mSelectionArgs = new ArrayList<>();
     	}
-    }
-
-    public SelectionBuilder mapToTable(String column, String table) {
-    	ensureProjectionMap();
-        mProjectionMap.put(column, table + "." + column);
-        return this;
     }
 
     public SelectionBuilder map(String fromColumn, String toClause) {
